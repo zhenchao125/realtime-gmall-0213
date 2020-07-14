4b015d4e055e995f00543f67490799d4dcab64a3
zhiling <zhiling@atguigu.com>
2020-07-14 09:36:51 +0800

    更新

---
+++ gmall-common/src/main/java/com/atguigu/common/Constant.java
@@ -0,0 +1,10 @@
+package com.atguigu.common;
+
+/**
+ * @Author lzc
+ * @Date 2020/7/14 9:29
+ */
+public class Constant {
+    public final static String STARTUP_TOPIC = "startup_topic";
+    public static final String EVENT_TOPIC = "event_topic";
+}
--- gmall-logger/src/main/java/com/atgugiu/gmalllogger/LoggerController.java
+++ gmall-logger/src/main/java/com/atgugiu/gmalllogger/LoggerController.java
@@ -1,5 +1,12 @@
 package com.atgugiu.gmalllogger;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.atguigu.common.Constant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +21,53 @@ public class LoggerController {
     // http://localhost:8080/log?log=abc
 
     @PostMapping("/log")
-    public String doLog(String log){
+    public String doLog(String log) {
         //1. 给日志添加一个时间戳
-
+        log = addTS(log);
         // 2. 数据落盘(为离线数据做准备)
-
+        saveToDisk(log);
         // 3. 把数据写入到kafka, 需要写入到topic
-
+        sendToKafka(log);
 
         return "ok";
+    }
+    @Autowired
+    KafkaTemplate kafka;
+    /**
+     * 把日志发送到kafka
+     * 不同的日志写入到不同的topic
+     *
+     * @param log
+     */
+    private void sendToKafka(String log) {
+        if (log.contains("\"startup\"")) {
+            kafka.send(Constant.STARTUP_TOPIC, log);
+        }else{
+            kafka.send(Constant.EVENT_TOPIC, log);
+        }
+    }
+
+    Logger logger = LoggerFactory.getLogger(LoggerController.class);
+
+    /**
+     * 把日志写入到磁盘
+     *
+     * @param log
+     */
+    private void saveToDisk(String log) {
+        logger.info(log);
+    }
+
+    /**
+     * 给日志添加时间戳
+     *
+     * @param log
+     * @return
+     */
+    private String addTS(String log) {
+        JSONObject obj = JSON.parseObject(log);
+        obj.put("ts", System.currentTimeMillis());
+        return JSON.toJSONString(obj);
     }
 
 
